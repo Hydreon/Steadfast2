c414412d3a80a6dbcc53300083f2245532e77934
Alexander Skosar <alleskos@gmail.com>
2016-05-26 16:11:34 +0700

    update point for compass

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1878,9 +1878,9 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer{
 //				$pk->spawnY = (int) $spawnPosition->y;
 //				$pk->spawnZ = (int) $spawnPosition->z;
 				/* hack for compass*/
-				$pk->spawnX = 1000000;
+				$pk->spawnX = -1000000;
 				$pk->spawnY = 10;
-				$pk->spawnZ = 1000000;
+				$pk->spawnZ = -1000000;
 				$pk->generator = 1; //0 old, 1 infinite, 2 flat
 				$pk->gamemode = $this->gamemode & 0x01;
 				$pk->eid = 0; //Always use EntityID as zero for the actual player
