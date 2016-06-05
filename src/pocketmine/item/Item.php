050e5edf18d9b72698da6f11a0de586af7a96f7d
DavidJBrockway <twitter@pocketserver.co.uk>
2016-06-05 09:27:00 +0100

    Update Item.php
    Removed Nether Reaactor

--- src/pocketmine/item/Item.php
+++ src/pocketmine/item/Item.php
@@ -912,7 +912,6 @@ class Item{
 		self::addCreativeItem(Item::get(Item::GLASS, 0));
 		self::addCreativeItem(Item::get(Item::GLOWSTONE_BLOCK, 0));
 		self::addCreativeItem(Item::get(Item::VINES, 0));
-		self::addCreativeItem(Item::get(Item::NETHER_REACTOR, 0));
 		self::addCreativeItem(Item::get(Item::LADDER, 0));
 		self::addCreativeItem(Item::get(Item::SPONGE, 0));
 		self::addCreativeItem(Item::get(Item::GLASS_PANE, 0));
