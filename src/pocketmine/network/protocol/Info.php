e394cbf19bf26d8505e0085ac78b72b4269f5679
Alexander Skosar <alleskos@gmail.com>
2016-07-29 13:24:02 +0700

    add new protocol

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -30,8 +30,8 @@ interface Info{
 	/**
 	 * Minecraft: PE protocol versions
 	 */
-	const CURRENT_PROTOCOL = 81;
-//	const NEWEST_PROTOCOL = 81;
+	const CURRENT_PROTOCOL = 82;
+//	const NEWEST_PROTOCOL = 82;
 //	const OLDEST_PROTOCOL = 39;
 	const ACCEPTED_PROTOCOLS = [40, 41, 42, 43, 44, 45, 46, 60, 70, 81, 82];
 
