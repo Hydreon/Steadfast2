d1fce074854d89872d2f3d4c83b89b84f63a2a55
RomanC <force@force-Aspire-E5-571G>
2016-11-28 16:54:38 +0600

    regen cancel bug

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -708,6 +708,7 @@ abstract class Entity extends Location implements Metadatable{
 	public function heal($amount, EntityRegainHealthEvent $source){
 		$this->server->getPluginManager()->callEvent($source);
 		if($source->isCancelled()){
+			$this->setHealth($this->health);
 			return;
 		}
 
