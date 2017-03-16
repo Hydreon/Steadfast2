ac546257db15906d1fad7deee8b5c3857e4b8151
RomanC <roman.estrange@gmail.com>
2017-03-16 15:01:38 +0600

    fixed onGround checking

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -1444,24 +1444,23 @@ abstract class Entity extends Location implements Metadatable{
 			}else{
 
 				if($this instanceof Player){
-					if(!$this->onGround or $movY != 0){
-						$bb = clone $this->boundingBox;
-						$bb->maxY = $bb->minY + 0.5;
-						$bb->minY -= 1;
-						if(count($this->level->getCollisionBlocks($bb, true)) > 0){
-							$this->onGround = true;
-						}else{
-							$this->onGround = false;
-						}					
+					$bb = clone $this->boundingBox;
+					$bb->maxY = $bb->minY + 0.5;
+					$bb->minY -= 1;
+					if(count($this->level->getCollisionBlocks($bb, true)) > 0){
+						echo "2";
+						$this->onGround = true;
+					}else{
+						$this->onGround = false;
+					}					
 //						
-//						$bb = clone $this->boundingBox;
-//						$bb->minY -= 1;
-//						if(count($this->level->getCollisionBlocks($bb)) > 0){
-//							$this->onGround = true;
-//						}else{
-//							$this->onGround = false;
-//						}
-					}
+//					$bb = clone $this->boundingBox;
+//					$bb->minY -= 1;
+//					if(count($this->level->getCollisionBlocks($bb)) > 0){
+//						$this->onGround = true;
+//					}else{
+//						$this->onGround = false;
+//					}
 					$this->isCollided = $this->onGround;
 				}else{
 					$this->isCollidedVertically = $movY != $dy;
