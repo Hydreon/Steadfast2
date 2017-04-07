abf2c222e0e0097c3d2692dd64ed6352abfda181
Alexander Skosar <alleskos@gmail.com>
2017-04-07 19:32:36 +0600

    add Map packets

---
+++ src/pocketmine/network/protocol/MapInfoRequestPacket.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace pocketmine\network\protocol;
+
+class MapInfoRequestPacket extends PEPacket {
+
+	const NETWORK_ID = Info::MAP_INFO_REQUEST_PACKET;
+	const PACKET_NAME = "MAP_INFO_REQUEST_PACKET";
+	
+	public $mapId;
+
+	public function decode($playerProtocol) {
+		$this->mapId = $this->getSignedVarInt();
+	}
+
+	public function encode($playerProtocol) {
+		
+	}
+
+}
