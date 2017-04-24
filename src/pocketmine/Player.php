bc69de90ac8acf3aef021b5e11c3d1016e067cf2
Alexander Skosar <alleskos@gmail.com>
2017-04-24 12:45:54 +0600

    1.1.0.8 support

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -4032,8 +4032,9 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer{
 	public function getPlayerProtocol() {
 		switch ($this->protocol) {
 			case ProtocolInfo::PROTOCOL_110:
+			case ProtocolInfo::PROTOCOL_111:
+				return ProtocolInfo::PROTOCOL_110;
 			case ProtocolInfo::PROTOCOL_105:
-				return $this->protocol;
 			case ProtocolInfo::PROTOCOL_106:
 			case ProtocolInfo::PROTOCOL_107:
 				return ProtocolInfo::PROTOCOL_105;
