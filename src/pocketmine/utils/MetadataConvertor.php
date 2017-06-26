8ecdf8df5ea052f3afff2c274f38f49df5fc0585
Kroer Max <kroer.m.d@gmail.com>
2017-06-26 14:38:30 +0600

    [120] changes in TextPacket, added flags in MetadataConverter

--- src/pocketmine/utils/MetadataConvertor.php
+++ src/pocketmine/utils/MetadataConvertor.php
@@ -24,7 +24,24 @@ class MetadataConvertor {
 		'DATA_FLAG_CHESTED_MOUNT' => 34,
 		'DATA_FLAG_STACKABLE' => 35,
 	];
+	private static $diffEntityFlags120 = [
+		'DATA_FLAG_RESTING_BAT' => 22,
+		'DATA_FLAG_ANIMAL_SIT' => 23,
+		'DATA_FLAG_ANGRY_WOLF' => 24,
+		'DATA_FLAG_INTERESTED' => 25,
+		'DATA_FLAG_ANGRY_BLAZE' => 26,
+		'DATA_FLAG_TAME_WOLF' => 27,
+		'DATA_FLAG_LEASHED' => 28,
+		'DATA_FLAG_SHAVED_SHIP' => 29,
+		'DATA_FLAG_FALL_FLYING' => 30,
+		'DATA_FLAG_ELDER_GUARDIAN' => 31,
+		'DATA_FLAG_MOVING' => 32,		
+		'DATA_FLAG_NOT_IN_WATER' => 33,
+		'DATA_FLAG_CHESTED_MOUNT' => 34,
+		'DATA_FLAG_STACKABLE' => 35,
+	];
 	private static $entityFlags110 = [];
+	private static $entityFlags120 = [];
 	private static $diffEntityMetaIds110 = [
 		'DATA_MAX_AIR' => 43,
 	];
@@ -46,9 +63,9 @@ class MetadataConvertor {
 			}
 		}
 
-		foreach (self::$diffEntityMetaIds110 as $key => $value) {
+		foreach (self::$diffEntityFlags120 as $key => $value) {
 			if (isset(self::$initialMeta[$key])) {
-				self::$entityMetaIds110[self::$initialMeta[$key]] = $value;
+				self::$entityFlags120[self::$initialMeta[$key]] = $value;
 			}
 		}
 		
@@ -94,23 +111,31 @@ class MetadataConvertor {
 	}
 
 	private static function updateEntityFlags($meta, $protocol) {
+		if (!isset($meta[Entity::DATA_FLAGS])) {
+			return $meta;
+		}
 		switch ($protocol) {
 			case Info::PROTOCOL_120:
+				$newflags = 1 << 19; //DATA_FLAG_CAN_CLIMBING
+				$protocolFlags = self::$entityFlags120;
+				break;
 			case Info::PROTOCOL_110:
-				if (isset($meta[Entity::DATA_FLAGS])) {
-					$newflags = 1 << 19; //DATA_FLAG_CAN_CLIMBING
-					$flags = strrev(decbin($meta[Entity::DATA_FLAGS][1]));
-					for ($i = 0; $i < strlen($flags); $i++) {
-						if ($flags{$i} === '1') {
-							$newflags |= 1 << (isset(self::$entityFlags110[$i]) ? self::$entityFlags110[$i] : $i);
-						}
-					}
-					$meta[Entity::DATA_FLAGS][1] = $newflags;
-				}
-				return $meta;
+				$newflags = 1 << 19; //DATA_FLAG_CAN_CLIMBING
+				$protocolFlags = self::$entityFlags110;
+				break;
 			default:
 				return $meta;
 		}
+		
+		$flags = strrev(decbin($meta[Entity::DATA_FLAGS][1]));
+		$flagsLength = strlen($flags);
+		for ($i = 0; $i < $flagsLength; $i++) {
+			if ($flags{$i} === '1') {
+				$newflags |= 1 << (isset($protocolFlags[$i]) ? $protocolFlags[$i] : $i);
+			}
+		}
+		$meta[Entity::DATA_FLAGS][1] = $newflags;
+		return $meta;
 	}
 
 }
