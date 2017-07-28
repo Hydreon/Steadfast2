e2abcab5f1947766fedd8b80d8e3ed6866ba4a77
Kroer Max <kroer.m.d@gmail.com>
2017-07-28 10:59:29 +0600

    [120] SimpleForm inmprovement (move handle logic from form to buttons)

--- src/pocketmine/customUI/elements/UIElement.php
+++ src/pocketmine/customUI/elements/UIElement.php
@@ -2,10 +2,20 @@
 
 namespace pocketmine\customUI\elements;
 
+use pocketmine\Player;
+
 abstract class UIElement {
 	
 	protected $text = '';
 	
+	/**
+	 * @return array
+	 */
 	abstract public function getDataToJson();
+	
+	/**
+	 * @param Player $player
+	 */
+	abstract public function handle($player);
 	
 }
