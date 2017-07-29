7dbb6d118f8b9f220a0326175b9bf2dd1abb6c7d
dishrex <drdischer@gmail.com>
2017-07-28 23:31:52 -0500

    add GUID to PONG for ping meter

--- src/raklib/protocol/UNCONNECTED_PING.php
+++ src/raklib/protocol/UNCONNECTED_PING.php
@@ -22,18 +22,20 @@ use raklib\RakLib;
 
 class UNCONNECTED_PING extends Packet{
     public static $ID = 0x01;
-
     public $pingID;
+    public $GUID;
 
     public function encode(){
         parent::encode();
         $this->putLong($this->pingID);
         $this->put(RakLib::MAGIC);
+        $this->putLong($this->GUID);
     }
 
     public function decode(){
         parent::decode();
         $this->pingID = $this->getLong();
-        //magic
+        $this->offset += 16; //magic
+        $this->GUID = $this->getLong();
     }
 }
