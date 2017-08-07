0cc39b5917574e95fce54dbd29d953d9c3216ec2
Kroer Max <kroer.m.d@gmail.com>
2017-08-07 12:32:43 +0600

    fix for fix

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -4381,6 +4381,7 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer{
 	 * @param string|null $data Sting in JSON format or null
 	 */
 	public function checkModal($formId, $data) {
+		if (isset($this->activeModalWindows[$formId])) {
 			if ($data === null) { // The modal window was closed manually
 				$this->activeModalWindows[$formId]->close($this);
 			} else { // Player send some data
