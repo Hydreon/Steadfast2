9eaf14c210a6366b1c051180d0497eecdfc4fa6f
Alexander Skosar <alleskos@gmail.com>
2017-09-11 13:39:21 +0600

    add 137 protocol support

--- src/pocketmine/network/protocol/PEPacket.php
+++ src/pocketmine/network/protocol/PEPacket.php
@@ -48,6 +48,7 @@ abstract class PEPacket extends DataPacket {
 			case Info::PROTOCOL_134:
 			case Info::PROTOCOL_135:
 			case Info::PROTOCOL_136:
+			case Info::PROTOCOL_137:
 				return Info::PROTOCOL_120;
 			case Info::PROTOCOL_110:
 			case Info::PROTOCOL_111:
