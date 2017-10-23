045d658bd18c48d3391aed24bc4e843bf08877c8
Kroer Max <kroer.m.d@gmail.com>
2017-10-23 14:12:46 +0600

    added new potocol (140)

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -33,7 +33,7 @@ interface Info{
 	const CURRENT_PROTOCOL = 101;
 //	const NEWEST_PROTOCOL = 82;
 //	const OLDEST_PROTOCOL = 81;
-	const ACCEPTED_PROTOCOLS = [101, 102, 105, 106, 107, 110, 111, 112, 113, 134, 135, 136, 137];
+	const ACCEPTED_PROTOCOLS = [101, 102, 105, 106, 107, 110, 111, 112, 113, 134, 135, 136, 137, 140];
 	
 	const BASE_PROTOCOL = 101;
 	/** RELEASE 1.0.x, 1.1.x */
@@ -49,6 +49,7 @@ interface Info{
 	const PROTOCOL_135 = 135; // 1.2.0.24, 1.2.0.25
 	const PROTOCOL_136 = 136; // 1.2.0.31
 	const PROTOCOL_137 = 137; // 1.2.0
+	const PROTOCOL_140 = 140; // 1.2.5.11
 	/** OUTDATED (supporting will be removed with next release, may didn't work properly)*/
 	const PROTOCOL_120 = 120; // 1.2.0.xx (beta)
 //	const PROTOCOL_121 = 121; // 1.2.0.xx (beta)
