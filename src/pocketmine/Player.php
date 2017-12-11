c888f6fe21bf649ab6c46287f367b6bffe37e13b
Kroer Max <kroer.m.d@gmail.com>
2017-12-11 15:17:30 +0600

    fix for craft from air

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -4174,6 +4174,7 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer{
 		} else if ($recipe instanceof ShapelessRecipe) {
 			$ingredients = $recipe->getIngredientList();
 		}
+		$isAllCraftSlotsEmpty = true;
 		$ingredientsCount = count($ingredients);
 		$firstIndex = 0;
 		foreach ($craftSlots as &$item) {
@@ -4191,6 +4192,7 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer{
 				if ($isItemsNotEquals) {
 					throw new \Exception('Recive bad recipe');
 				}
+				$isAllCraftSlotsEmpty = false;
 				$firstIndex = $i + 1;
 				$item->count -= $ingredient->count;
 				if ($item->count == 0) {
@@ -4199,6 +4201,9 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer{
 				}
 				break;
 			}
+		}
+		if ($isAllCraftSlotsEmpty) {
+			throw new \Exception('All craft slots are empty');
 		}
 	}
 
