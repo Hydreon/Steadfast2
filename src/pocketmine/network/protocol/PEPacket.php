14f09889723491d22849c72a0aac0c284f6a8af3
Kroer Max <kroer.m.d@gmail.com>
2018-04-06 14:51:22 +0600

    added support for 1.2.15.1

--- src/pocketmine/network/protocol/PEPacket.php
+++ src/pocketmine/network/protocol/PEPacket.php
@@ -55,6 +55,7 @@ abstract class PEPacket extends DataPacket {
 	public final static function convertProtocol($protocol) {
 		switch ($protocol) {
 			case Info::PROTOCOL_240:
+			case Info::PROTOCOL_250:
 				return Info::PROTOCOL_240;
 			case Info::PROTOCOL_221:
 			case Info::PROTOCOL_223:
