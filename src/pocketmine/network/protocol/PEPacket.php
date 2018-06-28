b28a3c0b77722dbf837445241e32f218cf75fec4
Alexander Skosar <alleskos@gmail.com>
2018-06-28 20:16:15 +0600

    protocol 281

--- src/pocketmine/network/protocol/PEPacket.php
+++ src/pocketmine/network/protocol/PEPacket.php
@@ -69,6 +69,7 @@ abstract class PEPacket extends DataPacket {
 	
 	public final static function convertProtocol($protocol) {
 		switch ($protocol) {
+			case Info::PROTOCOL_281:
 			case Info::PROTOCOL_280:
 				return Info::PROTOCOL_280;
 			case Info::PROTOCOL_274:
