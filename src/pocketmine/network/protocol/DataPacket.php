b1f35ddbd2c073dcf9d89301a978f9f81a2232b0
alleskos <alleskos@gmail.com>
2019-03-21 14:33:08 +0600

    protocol 354

--- src/pocketmine/network/protocol/DataPacket.php
+++ src/pocketmine/network/protocol/DataPacket.php
@@ -110,6 +110,7 @@ abstract class DataPacket extends BinaryStream{
 		self::$packetsIds[Info::PROTOCOL_342] = $oClass->getConstants();
 		self::$packetsIds[Info::PROTOCOL_350] = $oClass->getConstants();
 		self::$packetsIds[Info::PROTOCOL_351] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_354] = $oClass->getConstants();
 	}
 	
 }
