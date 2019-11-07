d78b101b94fcc62f6677e4be610c03ba731b56cf
iAldrich23xX <wilsonalejandro83@gmail.com>
2019-11-06 23:38:46 -0500

    Update PluginManager.php

--- src/pocketmine/plugin/PluginManager.php
+++ src/pocketmine/plugin/PluginManager.php
@@ -212,7 +212,7 @@ class PluginManager{
 								continue;
 							}
 
-							$compatible = false;
+							/*$compatible = false;
 							//Check multiple dependencies
 							foreach($description->getCompatibleApis() as $version){
 								//Format: majorVersion.minorVersion.patch
@@ -234,7 +234,7 @@ class PluginManager{
 							if($compatible === false){
 								$this->server->getLogger()->error("Could not load plugin '" . $name . "': API version not compatible");
 								continue;
-							}
+							}*/
 
 							$plugins[$name] = $file;
 
