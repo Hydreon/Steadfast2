5e4b7e057ba3a2b4af5ebe6453b462e5527163a9
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2019-11-17 09:49:46 +0200

    Update PocketMine.php

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -91,8 +91,8 @@ namespace pocketmine {
 		@define("pocketmine\\PATH", getcwd() . DIRECTORY_SEPARATOR);
 	}
 
-	if(version_compare("7.0", PHP_VERSION) > 0){
-		echo "[CRITICAL] You must use PHP >= 7.0" . PHP_EOL;
+	if(version_compare("7.2", PHP_VERSION) > 0){
+		echo "[CRITICAL] You must use PHP >= 7.2" . PHP_EOL;
 		echo "[CRITICAL] Please use the installer provided on the homepage." . PHP_EOL;
 		exit(1);
 	}
