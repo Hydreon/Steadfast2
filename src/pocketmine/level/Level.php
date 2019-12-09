26d6cddd1856f233342ab8e90136382c13641676
alleskos <alleskos@gmail.com>
2019-12-09 12:11:36 +0600

    isClosed method in level

--- src/pocketmine/level/Level.php
+++ src/pocketmine/level/Level.php
@@ -2412,4 +2412,8 @@ class Level implements ChunkManager, Metadatable{
 	public function getUpdateQueue() {
 		return $this->updateQueue;
 	}
+	
+	public function isClosed() {
+		return $this->closed;
+	}
 }
