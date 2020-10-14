2a5b91f75ca93a59deb09c1f1fa61f338923d254
Jacob Zukauska <jzukauska@outlook.com>
2020-10-14 11:22:55 -0500

    Added block breaking cancel and no on break for chest

--- src/pocketmine/level/Level.php
+++ src/pocketmine/level/Level.php
@@ -1188,6 +1188,22 @@ class Level implements ChunkManager, Metadatable{
 		}
 		$target = $this->getBlock($vector);
 		$drops = $target->getDrops($item); 
+
+
+		$tile = $this->getTile($target);
+
+
+		//if chest has viewers return false on use break on
+		if ($tile instanceof Chest) {
+			print_r($tile instanceof Chest);
+			if (count($tile->getInventory()->getViewers()) > 0) {				
+				return false;
+			}
+			
+		}
+		
+
+	
 		if ($player instanceof Player) {
 			if ($player->isSpectator() || !$player->canBreakBlocks()) {
 				return false;
