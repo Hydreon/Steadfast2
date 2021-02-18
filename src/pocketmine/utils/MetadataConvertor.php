a415e3c81729a4a45e8c4ec55ca56e6faa243514
dextudio <dextudio@dextudio.com>
2021-02-19 02:10:11 +0600

    start 428

--- src/pocketmine/utils/MetadataConvertor.php
+++ src/pocketmine/utils/MetadataConvertor.php
@@ -217,6 +217,7 @@ class MetadataConvertor {
 
 	private static function updateMetaIds($meta, $protocol) {
 		switch ($protocol) {
+            case Info::PROTOCOL_428:
             case Info::PROTOCOL_423:
             case Info::PROTOCOL_422:
             case Info::PROTOCOL_419:
@@ -286,6 +287,7 @@ class MetadataConvertor {
 			return $meta;
 		}
 		switch ($protocol) {
+            case Info::PROTOCOL_428:
             case Info::PROTOCOL_423:
             case Info::PROTOCOL_422:
             case Info::PROTOCOL_419:
