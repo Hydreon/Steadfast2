6f80f5f6cea8a73852d107b8d76d418b77884858
dextudio <dextudio@dextudio.com>
2021-05-20 21:43:53 +0600

    start protocol_440

--- src/pocketmine/network/Network.php
+++ src/pocketmine/network/Network.php
@@ -285,6 +285,7 @@ class Network {
 			case Info::PROTOCOL_423:	
 			case Info::PROTOCOL_428:
 			case Info::PROTOCOL_431:
+			case Info::PROTOCOL_440:
 				$class = $this->packetPool331[$id];
 				break;
 			case Info::PROTOCOL_310:
@@ -303,6 +304,7 @@ class Network {
 	
 	public static function getChunkPacketProtocol($playerProtocol){
 		switch ($playerProtocol) {
+			case Info::PROTOCOL_440:
 			case Info::PROTOCOL_431:
 			case Info::PROTOCOL_428:
 				return Info::PROTOCOL_428;
